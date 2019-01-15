63278a932b79a9dd1d31dff19acd8fbea5f60921
Ignasi Vallès <45039226+ignasivalles@users.noreply.github.com>
2019-01-15 18:44:41 +0100

    Update example_timelooping_motupython.sh

--- example_timelooping_motupython.sh
+++ example_timelooping_motupython.sh
@@ -34,11 +34,11 @@ tf=''$y'-'$m'-'$d' 12:00:00'
 tname=''$y''$m''$d'000000'
 
 # Download Loop 
-python /home/yourmotupythonfolder/motu-client-python-V16/motu-client.py --user $CMEMS_USERNAME --pwd $CMEMS_PWD...
---motu http://nrt.cmems-du.eu/motu-web/Motu --service-id GLOBAL_ANALYSIS_FORECAST_PHY_001_024-TDS...
---product-id global-analysis-forecast-phy-001-024...
---longitude-min -30 --longitude-max -5 --latitude-min 5 --latitude-max 44 --date-min $ti --date-max $tf...
---depth-min 0.493 --depth-max 109.7294 --variable thetao --variable so --variable uo --variable vo...
+python /home/yourmotupythonfolder/motu-client-python-V16/motu-client.py --user $CMEMS_USERNAME --pwd $CMEMS_PWD
+--motu http://nrt.cmems-du.eu/motu-web/Motu --service-id GLOBAL_ANALYSIS_FORECAST_PHY_001_024-TDS
+--product-id global-analysis-forecast-phy-001-024
+--longitude-min -30 --longitude-max -5 --latitude-min 5 --latitude-max 44 --date-min $ti --date-max $tf
+--depth-min 0.493 --depth-max 109.7294 --variable thetao --variable so --variable uo --variable vo
 --out-dir $OUTPUT_DIR --out-name "$FILENAME"_"$tname".nc
 
 echo $ti
